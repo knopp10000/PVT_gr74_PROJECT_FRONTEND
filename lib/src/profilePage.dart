9e03c99316e817e2ef5049b4528150f73aaea2c9
elzwanto <56538647+elzwanto@users.noreply.github.com>
2020-05-04 13:55:48 +0200

    Update profilePage.dart

--- lib/src/profilePage.dart
+++ lib/src/profilePage.dart
@@ -7,14 +7,79 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: color,
-      child: Center(
-        child: Text(
-          'Profile',
-          style: TextStyle(fontSize: 25),
+    final picture = Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: CircleAvatar(
+          radius: 120.0,
+          backgroundColor: Colors.transparent,
+          backgroundImage: AssetImage('assets/kaknas.jpg'),
         ),
       ),
     );
+
+    final vanner = Padding(
+      padding: EdgeInsets.symmetric(vertical: 6.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {
+          //Navigator.of(context).pushNamed();
+        },
+        color: Colors.lightBlueAccent,
+        padding: EdgeInsets.only(left: 120.0, right: 120.0, top: 15.0, bottom: 15.0),
+        child: Text('Mina vänner', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+    final badges = Padding(
+      padding: EdgeInsets.symmetric(vertical: 6.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {
+          //Navigator.of(context).pushNamed();
+        },
+        color: Colors.lightBlueAccent,
+        padding: EdgeInsets.only(left: 120.0, right: 120.0, top: 15.0, bottom: 15.0),
+        child: Text('Mina badges', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+    final bidrag = Padding(
+      padding: EdgeInsets.symmetric(vertical: 6.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {
+          //Navigator.of(context).pushNamed();
+        },
+        color: Colors.lightBlueAccent,
+        padding: EdgeInsets.only(left: 120.0, right: 120.0, top: 15.0, bottom: 15.0),
+        child: Text('Mina bidrag', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+    final body = Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.all(28.0),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Colors.blue,
+          Colors.lightBlueAccent,
+        ]),
+      ),
+      child: Column(
+        children: <Widget>[picture, vanner, badges, bidrag],
+      ),
+    );
+
+    return Scaffold(
+      body: body,
+    );
   }
-}
+  }
