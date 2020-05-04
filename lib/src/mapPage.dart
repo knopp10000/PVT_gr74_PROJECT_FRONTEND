048bcf52845c9c5a5135ec973987354a2d3a6945
alan4958 <alan4958@dsv.su.se>
2020-05-04 17:35:12 +0200

    marker

--- lib/src/mapPage.dart
+++ lib/src/mapPage.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -14,6 +15,11 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   Completer<GoogleMapController> _controller = Completer();
 
+  Map<MarkerId, Marker> markers = <MarkerId, Marker> {};
+  
+  int _markerIdCounter = 1;
+  MarkerId selectedMarker;
+
   static final CameraPosition _kStockholm = CameraPosition(
     target: LatLng(59.3253159, 18.0398813),
     zoom: 12,
@@ -26,7 +32,49 @@ class _MapPageState extends State<MapPage> {
       initialCameraPosition: _kStockholm,
       onMapCreated: (GoogleMapController controller) {
         _controller.complete(controller);
+        addMarker(const LatLng(59.3253159, 18.0398813));
+        addMarker(const LatLng(59.327, 18.0398813));
+      },
+      markers: Set<Marker>.of(markers.values),
+    );
+  }
+
+  void _onMarkerTapped(MarkerId markerId) {
+    final Marker tappedMarker = markers[markerId];
+    if (tappedMarker != null) {
+      setState(() {
+        if (markers.containsKey(selectedMarker)) {
+          final Marker resetOld = markers[selectedMarker]
+              .copyWith(iconParam: BitmapDescriptor.defaultMarker);
+          markers[selectedMarker] = resetOld;
+        }
+        selectedMarker = markerId;
+        final Marker newMarker = tappedMarker.copyWith(
+          iconParam: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueGreen,
+          ),
+        );
+        markers[markerId] = newMarker;
+      });
+    }
+  }
+
+  void addMarker(LatLng pos) {
+    final String markerIdVal = 'marker_id_$_markerIdCounter';
+    _markerIdCounter++;
+    final MarkerId markerId = MarkerId(markerIdVal);
+
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: pos,
+      infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
+      onTap: () {
+        _onMarkerTapped(markerId);
       },
     );
+
+    setState(() {
+      markers[markerId] = marker;
+    });
   }
 }
