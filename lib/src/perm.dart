c41dab7e9de81d81c197be5f28386336f4da2e0b
alan4958 <alan4958@dsv.su.se>
2020-05-04 22:29:06 +0200

    manifest is back

--- lib/src/perm.dart
+++ lib/src/perm.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:permission/permission.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class PermissionPage extends StatefulWidget {
   @override
@@ -9,296 +9,120 @@ class PermissionPage extends StatefulWidget {
 }
 
 class _PermissionPageState extends State<PermissionPage> {
-  bool a0 = false, a1 = false, a2 = false, a3 = false, a4 = false, a5 = false, a6 = false, a7 = false, a8 = false, a9 = false;
-  bool i0 = false, i1 = false, i2 = false, i3 = false, i4 = false, i5 = false, i6 = false;
-  int radioValue = 0;
-  PermissionName permissionName = PermissionName.Internet;
-  String message = '';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Plugin example app'),
+          title: const Text('Plugin example app'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () async {
+                var hasOpened = openAppSettings();
+                debugPrint('App Settings opened: ' + hasOpened.toString());
+              },
+            )
+          ],
         ),
         body: Center(
-          child: Column(
-            children: <Widget>[
-              Offstage(
-                offstage: !Platform.isAndroid,
-                child: Row(
-                  children: <Widget>[
-                    Checkbox(
-                      value: a0,
-                      onChanged: (v) {
-                        setState(() {
-                          a0 = v;
-                        });
-                      },
-                    ),
-                    Text('Calendar'),
-                    Checkbox(
-                      value: a1,
-                      onChanged: (v) {
-                        setState(() {
-                          a1 = v;
-                        });
-                      },
-                    ),
-                    Text('Camera'),
-                    Checkbox(
-                      value: a2,
-                      onChanged: (v) {
-                        setState(() {
-                          a2 = v;
-                        });
-                      },
-                    ),
-                    Text('Contacts'),
-                  ],
-                ),
-              ),
-              Offstage(
-                offstage: !Platform.isAndroid,
-                child: Row(
-                  children: <Widget>[
-                    Checkbox(
-                      value: a3,
-                      onChanged: (v) {
-                        setState(() {
-                          a3 = v;
-                        });
-                      },
-                    ),
-                    Text('Microphone'),
-                    Checkbox(
-                      value: a4,
-                      onChanged: (v) {
-                        setState(() {
-                          a4 = v;
-                        });
-                      },
-                    ),
-                    Text('Location'),
-                    Checkbox(
-                      value: a5,
-                      onChanged: (v) {
-                        setState(() {
-                          a5 = v;
-                        });
-                      },
-                    ),
-                    Text('Phone'),
-                  ],
-                ),
-              ),
-              Offstage(
-                offstage: !Platform.isAndroid,
-                child: Row(
-                  children: <Widget>[
-                    Checkbox(
-                      value: a6,
-                      onChanged: (v) {
-                        setState(() {
-                          a6 = v;
-                        });
-                      },
-                    ),
-                    Text('Sensors'),
-                    Checkbox(
-                      value: a7,
-                      onChanged: (v) {
-                        setState(() {
-                          a7 = v;
-                        });
-                      },
-                    ),
-                    Text('SMS'),
-                    Checkbox(
-                      value: a8,
-                      onChanged: (v) {
-                        setState(() {
-                          a8 = v;
-                        });
-                      },
-                    ),
-                    Text('Storage'),
-                  ],
-                ),
-              ),
-              Offstage(
-                offstage: !Platform.isAndroid,
-                child: Row(
-                  children: <Widget>[
-                    Checkbox(
-                      value: a9,
-                      onChanged: (v) {
-                        setState(() {
-                          a9 = v;
-                        });
-                      },
-                    ),
-                    Text('State'),
-                  ],
-                ),
-              ),
-              Offstage(
-                offstage: !Platform.isIOS,
-                child: Row(
-                  children: <Widget>[
-                    Checkbox(
-                      value: i0,
-                      onChanged: (v) {
-                        setState(() {
-                          i0 = v;
-                        });
-                      },
-                    ),
-                    Text('Internet'),
-                    Checkbox(
-                      value: i1,
-                      onChanged: (v) {
-                        setState(() {
-                          i1 = v;
-                        });
-                      },
-                    ),
-                    Text('Calendar'),
-                    Checkbox(
-                      value: i2,
-                      onChanged: (v) {
-                        setState(() {
-                          i2 = v;
-                        });
-                      },
-                    ),
-                    Text('Camera'),
-                  ],
-                ),
-              ),
-              Offstage(
-                offstage: !Platform.isIOS,
-                child: Row(
-                  children: <Widget>[
-                    Checkbox(
-                      value: i3,
-                      onChanged: (v) {
-                        setState(() {
-                          i3 = v;
-                        });
-                      },
-                    ),
-                    Text('Contacts'),
-                    Checkbox(
-                      value: i4,
-                      onChanged: (v) {
-                        setState(() {
-                          i4 = v;
-                        });
-                      },
-                    ),
-                    Text('Microphone'),
-                    Checkbox(
-                      value: i5,
-                      onChanged: (v) {
-                        setState(() {
-                          i5 = v;
-                        });
-                      },
-                    ),
-                    Text('Location'),
-                  ],
-                ),
-              ),
-              Offstage(
-                offstage: !Platform.isIOS,
-                child: Row(
-                  children: <Widget>[
-                    Checkbox(
-                      value: i6,
-                      onChanged: (v) {
-                        setState(() {
-                          i6 = v;
-                        });
-                      },
-                    ),
-                    Text('Storage'),
-                  ],
-                ),
-              ),
-              RaisedButton(onPressed: getPermissionsStatus, child: new Text("Get permission status")),
-              RaisedButton(onPressed: requestPermissions, child: new Text("Request permissions")),
-              RaisedButton(onPressed: Permission.openSettings, child: new Text("Open settings")),
-              Text(message),
-            ],
-          ),
+          child: ListView(
+              children: Permission.values
+                  .where((Permission permission) {
+                    if (Platform.isIOS) {
+                      return permission != Permission.unknown &&
+                          permission != Permission.sms &&
+                          permission != Permission.storage &&
+                          permission != Permission.ignoreBatteryOptimizations &&
+                          permission != Permission.accessMediaLocation;
+                    } else {
+                      return permission != Permission.unknown &&
+                          permission != Permission.mediaLibrary &&
+                          permission != Permission.photos &&
+                          permission != Permission.reminders;
+                    }
+                  })
+                  .map((permission) => PermissionWidget(permission))
+                  .toList()),
         ),
       ),
     );
   }
+}
 
-  getPermissionsStatus() async {
-    List<PermissionName> permissionNames = [];
-    if (a0) permissionNames.add(PermissionName.Calendar);
-    if (a1) permissionNames.add(PermissionName.Camera);
-    if (a2) permissionNames.add(PermissionName.Contacts);
-    if (a3) permissionNames.add(PermissionName.Microphone);
-    if (a4) permissionNames.add(PermissionName.Location);
-    if (a5) permissionNames.add(PermissionName.Phone);
-    if (a6) permissionNames.add(PermissionName.Sensors);
-    if (a7) permissionNames.add(PermissionName.SMS);
-    if (a8) permissionNames.add(PermissionName.Storage);
+/// Permission widget which displays a permission and allows users to request
+/// the permissions.
+class PermissionWidget extends StatefulWidget {
+  /// Constructs a [PermissionWidget] for the supplied [Permission].
+  const PermissionWidget(this._permission);
 
-    if (i0) permissionNames.add(PermissionName.Internet);
-    if (i1) permissionNames.add(PermissionName.Calendar);
-    if (i2) permissionNames.add(PermissionName.Camera);
-    if (i3) permissionNames.add(PermissionName.Contacts);
-    if (i4) permissionNames.add(PermissionName.Microphone);
-    if (i5) permissionNames.add(PermissionName.Location);
-    if (i6) permissionNames.add(PermissionName.Storage);
-    message = '';
-    List<Permissions> permissions = await Permission.getPermissionsStatus(permissionNames);
-    permissions.forEach((permission) {
-      message += '${permission.permissionName}: ${permission.permissionStatus}\n';
-    });
-    setState(() {
-      message;
-    });
+  final Permission _permission;
+
+  @override
+  _PermissionState createState() => _PermissionState(_permission);
+}
+
+class _PermissionState extends State<PermissionWidget> {
+  _PermissionState(this._permission);
+
+  final Permission _permission;
+  PermissionStatus _permissionStatus = PermissionStatus.undetermined;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _listenForPermissionStatus();
   }
 
-  getSinglePermissionStatus() async {
-    var permissionStatus = await Permission.getSinglePermissionStatus(permissionName);
-    setState(() {
-      message = permissionStatus.toString();
-    });
+  void _listenForPermissionStatus() async {
+    final status = await _permission.status;
+    setState(() => _permissionStatus = status);
   }
 
-  requestPermissions() async {
-    List<PermissionName> permissionNames = [];
-    if (a0) permissionNames.add(PermissionName.Calendar);
-    if (a1) permissionNames.add(PermissionName.Camera);
-    if (a2) permissionNames.add(PermissionName.Contacts);
-    if (a3) permissionNames.add(PermissionName.Microphone);
-    if (a4) permissionNames.add(PermissionName.Location);
-    if (a5) permissionNames.add(PermissionName.Phone);
-    if (a6) permissionNames.add(PermissionName.Sensors);
-    if (a7) permissionNames.add(PermissionName.SMS);
-    if (a8) permissionNames.add(PermissionName.Storage);
-    if (a9) permissionNames.add(PermissionName.State);
+  Color getPermissionColor() {
+    switch (_permissionStatus) {
+      case PermissionStatus.denied:
+        return Colors.red;
+      case PermissionStatus.granted:
+        return Colors.green;
+      default:
+        return Colors.grey;
+    }
+  }
 
-    if (i0) permissionNames.add(PermissionName.Internet);
-    if (i1) permissionNames.add(PermissionName.Calendar);
-    if (i2) permissionNames.add(PermissionName.Camera);
-    if (i3) permissionNames.add(PermissionName.Contacts);
-    if (i4) permissionNames.add(PermissionName.Microphone);
-    if (i5) permissionNames.add(PermissionName.Location);
-    if (i6) permissionNames.add(PermissionName.Storage);
-    message = '';
-    var permissions = await Permission.requestPermissions(permissionNames);
-    permissions.forEach((permission) {
-      message += '${permission.permissionName}: ${permission.permissionStatus}\n';
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(_permission.toString()),
+      subtitle: Text(
+        _permissionStatus.toString(),
+        style: TextStyle(color: getPermissionColor()),
+      ),
+      trailing: IconButton(
+          icon: const Icon(Icons.info),
+          onPressed: () {
+            checkServiceStatus(context, _permission);
+          }),
+      onTap: () {
+        requestPermission(_permission);
+      },
+    );
+  }
+
+  void checkServiceStatus(BuildContext context, Permission permission) async {
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text((await permission.status).toString()),
+    ));
+  }
+
+  Future<void> requestPermission(Permission permission) async {
+    final status = await permission.request();
+
+    setState(() {
+      print(status);
+      _permissionStatus = status;
+      print(_permissionStatus);
     });
-    setState(() {});
   }
 }
