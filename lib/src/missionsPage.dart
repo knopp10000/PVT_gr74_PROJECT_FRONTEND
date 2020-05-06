39e3acb1865e6f63ee0aa5b5eab4c8cb00b42e1e
alan4958 <alan4958@dsv.su.se>
2020-05-06 22:06:00 +0200

    Uppdatering missionsPage

--- lib/src/missionsPage.dart
+++ lib/src/missionsPage.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class MissionsPage extends StatelessWidget {
@@ -5,165 +7,94 @@ class MissionsPage extends StatelessWidget {
 
   MissionsPage(this.color);
 
-  @override
-  Widget build(BuildContext context) {
+  Widget _missionButton(String title) {
+    return Container(
+      width: 280.0,
+      height: 55.0,
+      margin: EdgeInsets.all(8.0),
+        child: RaisedButton(
+          padding: EdgeInsets.symmetric(horizontal: 28.0, vertical: 8.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          onPressed: () {
+            //Navigator.of(context).pushNamed();
+          },
+          color: Colors.red[200],
+          child: Text(title, style: TextStyle(color: Colors.white)),
+        ));
+  }
 
-    final text1 = Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text(
-        'Populära uppdrag nära dig',
-        style: TextStyle(fontSize: 25.0, color: Colors.white),
-      ),
-    );
-
-    final runda1 = Container(
-      width: 325.0,
-      height: 60.0,
-      padding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 6.0),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
-        ),
-        onPressed: () {
-          //Navigator.of(context).pushNamed();
-        },
-        color: Colors.lightBlueAccent,
-        child: Text('Runda 1', style: TextStyle(color: Colors.white)),
-      ),
-    );
-
-    final runda2 = Container(
-      width: 325.0,
-      height: 60.0,
-      padding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 6.0),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
-        ),
-        onPressed: () {
-          //Navigator.of(context).pushNamed();
-        },
-        color: Colors.lightBlueAccent,
-        child: Text('Runda 2', style: TextStyle(color: Colors.white)),
-      ),
-    );
-
-    final runda3 = Container(
-      width: 325.0,
-      height: 60.0,
-      padding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 6.0),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
-        ),
-        onPressed: () {
-          //Navigator.of(context).pushNamed();
-        },
-        color: Colors.lightBlueAccent,
-        child: Text('Runda 3', style: TextStyle(color: Colors.white)),
-      ),
-    );
-
-    final runda4 = Container(
-      width: 325.0,
-      height: 60.0,
-      padding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 6.0),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
-        ),
-        onPressed: () {
-          //Navigator.of(context).pushNamed();
-        },
-        color: Colors.lightBlueAccent,
-        child: Text('Runda 4', style: TextStyle(color: Colors.white)),
-      ),
-    );
-
-    final container = Container(
-      //padding: const EdgeInsets.all(32.0),
-        height: 320.0,
-        margin: const EdgeInsets.only(bottom: 5.0),
-        decoration: new BoxDecoration(
-            color: Colors.white70.withOpacity(0.3), //new Color.fromRGBO(255, 0, 0, 0.0),
-            borderRadius: new BorderRadius.only(
-                topLeft:  const  Radius.circular(40.0),
-                topRight: const  Radius.circular(40.0),
-                bottomLeft:  const  Radius.circular(40.0),
-                bottomRight: const  Radius.circular(40.0))
-        ),
-        child: Column(
-          children: <Widget>[text1, runda1, runda2, runda3, runda4],
-        )
-    );
-
-    final skapa = Container(
+  Widget _actionButton(String title, Color color) {
+    return Container(
       width: 330.0,
       height: 60.0,
       padding: EdgeInsets.symmetric(vertical: 6.0),
       child: RaisedButton(
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
+          borderRadius: BorderRadius.circular(12),
         ),
         onPressed: () {
           //Navigator.of(context).pushNamed();
         },
-        color: Colors.deepOrangeAccent,
-        padding: EdgeInsets.only(left: 100.0, right: 100.0, top: 15.0, bottom: 15.0),
-        child: Text('Skapa nytt uppdrag', style: TextStyle(color: Colors.white)),
-      ),
-    );
-
-    final avklarade = Container(
-      width: 330.0,
-      height: 60.0,
-      padding: EdgeInsets.symmetric(vertical: 6.0),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
+        color: color,
+        child: Text(
+          title,
+          style: TextStyle(color: Colors.white),
+          textAlign: TextAlign.center,
         ),
-        onPressed: () {
-          //Navigator.of(context).pushNamed();
-        },
-        color: Colors.red,
-        padding: EdgeInsets.only(left: 75.0, right: 75.0, top: 15.0, bottom: 15.0),
-        child: Text('Alla mina avklarade uppdrag', style: TextStyle(color: Colors.white)),
       ),
     );
+  }
 
-    final noName = Container(
-      width: 330.0,
-      height: 60.0,
-      padding: EdgeInsets.symmetric(vertical: 6.0),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
-        ),
-        onPressed: () {
-          //Navigator.of(context).pushNamed();
-        },
-        color: Colors.green,
-        padding: EdgeInsets.only(left: 107.0, right: 107.0, top: 15.0, bottom: 15.0),
-        child: Text('Inte namngiven än', style: TextStyle(color: Colors.white)),
-      ),
-    );
-
-    final body = Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.all(28.0),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Colors.blue,
-          Colors.lightBlueAccent,
-        ]),
+  Widget _missionListView() {
+    return Container(
+      padding: EdgeInsets.only(top: 8.0, bottom: 12.0, left: 20.0, right: 20.0),
+      margin: const EdgeInsets.symmetric(vertical: 20.0),
+      decoration: new BoxDecoration(
+        color: Colors.white54
+            .withOpacity(0.3), //new Color.fromRGBO(255, 0, 0, 0.0),
+        borderRadius: new BorderRadius.all(Radius.circular(12)),
       ),
       child: Column(
-        children: <Widget>[container, skapa, avklarade, noName],
+        children: <Widget>[
+          Text(
+            'Populära uppdrag nära dig',
+            style: TextStyle(fontSize: 22.0, color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
+          _missionButton('Runda 1'),
+          _missionButton('Runda 2'),
+          _missionButton('Runda 3'),
+          _missionButton('Runda 4'),
+        ],
       ),
     );
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: body,
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.all(28.0),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.blue,
+              Colors.lightBlueAccent,
+            ]),
+          ),
+          child: Column(
+            children: <Widget>[
+              _missionListView(),
+              _actionButton('Skapa nytt uppdrag', Colors.deepOrangeAccent),
+              _actionButton('Utförda uppdrag', Colors.red),
+              _actionButton('Syfteslös knapp', Colors.green),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
