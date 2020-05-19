3c0b26a765d2413c55fe75b4d1dc38b8f9eccdd0
svant <svante_royen@hotmail.com>
2020-05-19 20:57:18 +0200

    Notifikation

---
+++ lib/src/pages/Messaging.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:meta/meta.dart';
+
+class Messaging {
+  static final Client client = Client();
+
+  // from 'https://console.firebase.google.com'
+  // --> project settings --> cloud messaging --> "Server key"
+  static const String serverKey =
+      'AAAA58dOr-w:APA91bFg6vJEyppGKAr8exc1qgB06tWFPorwR3zGgSm6X_c_IXL4HtDQqUEB08JX3ny-NBmzZT7Ylh99WTaGMBPr-WIM5AAcnIYmSBVHiFQu_5Ua9GazuIdHHp_rGBtCvQhozBoVREU0';
+
+  static Future<Response> sendToAll({
+    @required String title,
+    @required String body,
+  }) =>
+      sendToTopic(title: title, body: body, topic: 'all');
+
+  static Future<Response> sendToTopic(
+      {@required String title,
+        @required String body,
+        @required String topic}) =>
+      sendTo(title: title, body: body, fcmToken: '/topics/$topic');
+
+  static Future<Response> sendTo({
+    @required String title,
+    @required String body,
+    @required String fcmToken,
+  }) =>
+      client.post(
+        'https://fcm.googleapis.com/fcm/send',
+        body: json.encode({
+          'notification': {'body': '$body', 'title': '$title'},
+          'priority': 'high',
+          'data': {
+            'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+            'id': '1',
+            'status': 'done',
+          },
+          'to': '$fcmToken',
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'key=$serverKey',
+        },
+      );
+}
--- lib/src/pages/MessagingWidget.dart
+++ lib/src/pages/MessagingWidget.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:history_go/src/pages/Message.dart';
+import 'package:history_go/src/pages/Messaging.dart';
+
 
 class MessagingWidget extends StatefulWidget {
   @override
@@ -9,19 +11,29 @@ class MessagingWidget extends StatefulWidget {
 
 class _MessagingWidgetState extends State<MessagingWidget> {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  final TextEditingController titleController =
+  TextEditingController(text: 'Title');
+  final TextEditingController bodyController =
+  TextEditingController(text: 'Body');
   final List<Message> messages = [];
 
   @override
   void initState() {
     super.initState();
+    _firebaseMessaging.onTokenRefresh.listen(sendTokenToServer);
+    _firebaseMessaging.getToken();
+
+    _firebaseMessaging.subscribeToTopic('all');
+
     _firebaseMessaging.configure(
-      // Körs när appen körs i förgrunden
-      onMessage: (Map<String, dynamic> message) async {
+    // Körs när appen körs i förgrunden
+    onMessage: (Map<String, dynamic> message) async {
         print("onMessage: $message");
         final notification = message['notification'];
         setState(() {
           messages.add(Message(
-              title: notification['title'], body: notification['body']));
+              title: notification['title'],
+              body: notification['body']));
         });
       },
       // Körs när appen är stängd
@@ -46,11 +58,45 @@ class _MessagingWidgetState extends State<MessagingWidget> {
 
   @override
   Widget build(BuildContext context) => ListView(
-    children: messages.map(buildMessage).toList(),
+    children: [
+      TextFormField(
+        controller: titleController,
+        decoration: InputDecoration(labelText: 'Title'),
+      ),
+      TextFormField(
+        controller: bodyController,
+        decoration: InputDecoration(labelText: 'Body'),
+      ),
+      RaisedButton(
+        onPressed: sendNotification,
+        child: Text('Send notification'),
+      ),
+    ]..addAll(messages.map(buildMessage).toList()),
   );
 
   Widget buildMessage(Message message) => ListTile(
     title: Text(message.title),
     subtitle: Text(message.body),
   );
+
+  Future sendNotification() async {
+    final response = await Messaging.sendToAll(
+      title: titleController.text,
+      body: bodyController.text,
+      // fcmToken: fcmToken,
+    );
+
+    if (response.statusCode != 200) {
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content:
+        Text('[${response.statusCode}] Error message: ${response.body}'),
+      ));
+    }
+  }
+
+  void sendTokenToServer(String fcmToken) {
+    print('Token: $fcmToken');
+    // send key to your server to allow server to use
+    // this token to send push notifications
+  }
 }
